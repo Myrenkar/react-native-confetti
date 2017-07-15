d0a05afe71e8332cf7fc32ff1a28c2a072708bc9
Myrenkar <piotr.torczynski@gmail.com>
2017-07-15 19:52:39 +0200

    Added Wifi check

---
+++ custom_modules/react-native-wifi-state/iOS/RNWifiState/WifiState.h
@@ -0,0 +1,16 @@
+//
+//  WifiState.h
+//  RNWifiState
+//
+//  Created by Piotr Torczynski on 15/07/2017.
+//  Copyright © 2017 Netguru. All rights reserved.
+//
+
+@import Foundation;
+
+@interface WifiState : NSObject
+
+
+- (BOOL)isConnectedToWiFi;
+
+@end
